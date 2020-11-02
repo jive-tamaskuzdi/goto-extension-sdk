9758002025524713e39b5a94ab1bea8aadaf262d
David Mezei <david.mezei@logmein.com>
2020-11-02 22:55:57 +0100

    module

--- goto-extension-sdk.js
+++ goto-extension-sdk.js
@@ -1,40 +1,38 @@
-window.GoToExtensionSDK = (() => {
-  const handlers = {};
+const handlers = {};
 
-  window.addEventListener('message', (event => {
-    //TODO check event.origin if it's a valid source
+window.addEventListener('message', (event => {
+  //TODO check event.origin if it's a valid source
 
-    if (event.type !== 'GOTO___ONCLICK') {
-      return;
-    }
-
-
-    if (handlers[event.button]) {
-      for (const handler of handlers[event.button]) {
-        handler();
-      }
-    }
-  }));
-
-
-  function onButtonClick(name, callback) {
-    if (!handlers[name]) {
-      handlers[name] = [];
-    }
-    handlers[name].push(callback);
+  if (event.type !== 'GOTO___ONCLICK') {
+    return;
   }
 
-  function shareContent(url) {
-    if (window.parent) {
-      window.parent.postMessage({
-        type: 'GOTO___SHOW_CONTENT',
-        url: url
-      }, '*') //TODO whitelisted origins only
+
+  if (handlers[event.button]) {
+    for (const handler of handlers[event.button]) {
+      handler();
     }
   }
+}));
 
-  return {
+
+function onButtonClick(name, callback) {
+  if (!handlers[name]) {
+    handlers[name] = [];
+  }
+  handlers[name].push(callback);
+}
+
+function shareContent(url) {
+  if (window.parent) {
+    window.parent.postMessage({
+      type: 'GOTO___SHOW_CONTENT',
+      url: url
+    }, '*') //TODO whitelisted origins only
+  }
+}
+
+export const GoToExtensionSDK = {
     onButtonClick: onButtonClick,
     shareContent: shareContent,
-  }
-});
+};
